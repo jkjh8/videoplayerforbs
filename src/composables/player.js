57d3874acf98f3b84c41e280b02ddc9d0bd376c9
jkjh8 <jkjh82@naver.com>
2022-05-21 10:48:08 +0900

    first

---
+++ src/composables/player.js
@@ -0,0 +1,13 @@
+import { ref } from 'vue'
+
+export const mediaplayer = ref(null)
+export const autoplay = ref(false)
+export const bottomControls = ref(false)
+export const controlDisplayTime = ref(2000)
+export const showBigPlayBtn = ref(false)
+export const loop = ref(false)
+export const muted = ref(false)
+export const noControls = ref(false)
+export const volume = ref(60)
+export const showTooltips = ref(false)
+export const source = ref('')
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,94 @@
+<script setup>
+import { ref } from 'vue'
+import { QMediaPlayer } from '@quasar/quasar-ui-qmediaplayer'
+import '@quasar/quasar-ui-qmediaplayer/src/index.sass'
+import {
+  mediaplayer,
+  autoplay,
+  bottomControls,
+  controlDisplayTime,
+  showBigPlayBtn,
+  loop,
+  muted,
+  noControls,
+  volume,
+  showTooltips,
+  source
+} from 'src/composables/player'
+
+function ready() {
+  console.log('Ready')
+}
+
+function duration(sec) {
+  console.log('Duration', sec)
+}
+
+function ended() {
+  console.log('Ended')
+}
+
+function error(err) {
+  console.error('Media Player Error', err)
+}
+
+function paused() {
+  console.log('Paused')
+}
+
+function playing() {
+  console.log('Playing')
+}
+
+function timeupdate(curTime, remaining) {
+  console.log('timeupdated', curTime, remaining)
+}
+
+function fullscreen(state) {
+  console.log('Fullscreen', state)
+}
+
+function loadeddata() {
+  console.log('Loaded Data')
+}
+
+function showControls(state) {
+  console.log('Show Controls', state)
+}
+
+function getVolume(vol) {
+  console.log('ChangeVolume', vol)
+}
+
+function getMuted(state) {
+  console.log('Muted', state)
+}
+</script>
+
+<template>
+  <q-media-player
+    ref="mediaplayer"
+    type="video"
+    :source="source"
+    :autoplay="autoplay"
+    :bottom-controls="bottomControls"
+    :controls-display-time="controlDisplayTime"
+    :loop="loop"
+    :muted="muted"
+    :no-controls="noControls"
+    :show-big-play-button="showBigPlayBtn"
+    :volume="volume"
+    :show-tooltips="showTooltops"
+    @ready="ready"
+    @duration="duration"
+    @ended="ended"
+    @error="error"
+    @paused="paused"
+    @timeupdate="timeupdate"
+    @fullscreen="fullscreen"
+    @loadeddata="loadeddata"
+    @showControls="showControls"
+    @volume="getVolume"
+    @mute="getMuted"
+  />
+</template>
